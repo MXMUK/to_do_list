26dfe39e9e13aed8f1bb94d181f1cb7554541324
Roman <romamaksymuk@gmail.com>
2022-02-14 23:11:22 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Fcomponent from './Fcomponent';
-import Cconponent from './Cconponent';
-import Menu from './Menu';
-
-
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Fcomponent />
-        <Cconponent />
-        <Menu />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world привіт всім їїї
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ALL
-        </a>
-      </header>
+    <div>
+      working all
     </div>
   );
 }
