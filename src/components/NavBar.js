feacf9219a3787fc0680b8a438476f88019da176
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-02-18 16:17:09 +0200

    add nav bar commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+require("./styleNavBar.css")
+require("./animationnavbar")
+
+
+function NavBar() {
+  return (
+    <React.Fragment>
+     
+     <h1>ffffffffff1</h1>
+     <h1>ffffffffff1</h1>
+     <h1>ffffffffff1</h1>
+     <h1>ffffffffff1</h1>
+     <h1>ffffffffff1</h1>
+
+    </React.Fragment>
+  );
+}
+export default NavBar;
