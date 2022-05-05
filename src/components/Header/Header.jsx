5507c23a32e47c4bc5903407253ff6bdd8b9a3f8
Roman <romamaksymuk@gmail.com>
2022-05-05 12:57:07 +0300

    menu-dine

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,25 +9,33 @@ import menuImg from '../../assets/img/icons/header_panel/menu.svg'
 import Menu from './menu/Menu.jsx'
 
 
+
 function Header() {
 
-  const [menuActive, setMenuActive] = useState(false);
+  let [menuActive, setMenuActive] = useState(false);
 
   return (
     <div className={styles.header}>
       <div className={styles.wrapper}>
-        <div className={styles.menu}>
+        <div className={styles.menu_space}>
             <button className={styles.burger_button} onClick={() => setMenuActive(!menuActive)}>
-              <img src={menuImg} alt="icon" height='28'/>
+              <img src={menuImg} alt="menu" height='28'/>
+
             </button>
             <Menu active={menuActive} setActive={setMenuActive}/>
         </div>
+        <div className={styles.form}>
+          <form className={styles.search_form}>
+            <input type="text" placeholder='Quick find' className={styles.search_input} onChange={(event) => console.log(event.target.value)}/>
+          </form>
+        </div>
+
 
         <ul className={styles.panel}>
-            <li className={styles.panel_item}><img src={plusImg} alt="icon" height='28'/></li>
-            <li className={styles.panel_item}><img src={doneImg} alt="icon" height='28'/></li>
-            <li className={styles.panel_item}><img src={bellImg} alt="icon" height='28'/></li>
-            <li className={styles.panel_item}><img src={settingsImg} alt="icon" height='28'/></li>
+            <li className={styles.panel_item}><img src={plusImg} alt="icon" height='24'/></li>
+            <li className={styles.panel_item}><img src={doneImg} alt="icon" height='24'/></li>
+            <li className={styles.panel_item}><img src={bellImg} alt="icon" height='24'/></li>
+            <li className={styles.panel_item}><img src={settingsImg} alt="icon" height='24'/></li>
         </ul>
       </div>
     </div>
