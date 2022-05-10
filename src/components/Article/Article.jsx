b8465fbebd44e06f6ebd8132d0ec53f3b9d3cc1a
Roman <romamaksymuk@gmail.com>
2022-05-10 08:16:32 +0300

    tested_weather

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import './Article_module.scss'
+import Main from "../Main/Main";
+
 
 function Article({active}) {
   return (
     <div className='article'>
         <div className={active ? 'wrapper smaller' : 'wrapper'}>
             <div className="main">
+                <Main />
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati maiores ducimus natus dolorem ad cupiditate, sint quia! Repellendus, exercitationem natus? Veniam rerum dolores suscipit ratione error molestiae, ullam voluptates sed?
                 Asperiores minima, deleniti ducimus nulla sequi delectus corrupti. Debitis mollitia ut voluptas asperiores cum animi quod qui vitae esse unde, minus illo doloremque aperiam. Dignissimos facere blanditiis distinctio fugiat excepturi?
                 Adipisci provident fugit autem. Obcaecati aperiam cumque laboriosam pariatur voluptatem perferendis ducimus dignissimos dicta enim veniam, natus repudiandae explicabo dolore, voluptas, dolor quam nemo non quaerat vitae? Asperiores, corrupti quos?
